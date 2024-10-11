05f44843ae81c68688acef9907e15bff07046996
branmkim <bran.mkim@gmail.com>
2024-10-10 20:03:49 -0400

    updated filler text

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
         <Navbar />
         <div className="section1">
             <h1 className="headline">Networking for Creatives</h1>
-            <p className="bodytext">penis cock balls shaft penis cock balls shaft penis cock balls shaft penis cock balls shaft penis cock balls shaft penis cock balls shaft penis cock balls shaft penis cock balls shaft penis cock balls shaft penis cock balls shaft</p>
+            <p className="bodytext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
             <h3>
                 <Button text="Make an Account" variant="filled" />
             </h3>
@@ -29,21 +30,21 @@ function App() {
             <div className="card-container">
                 <div className="card">
                     <h2 style={{fontSize: "2.5rem"}}>Developed by models for models</h2>
-                    <p>wow sophie is such a bad ta for showing up to her meeting 10 minutes late and forgetting that she evne had it she is so bad at being a responsible ta</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                 </div>
                 <div className="card">
                     {/* TODO: entrepreneurship is too long of a word */}
                     <h2 style={{fontSize: "2.5rem"}}>NYU Entrepreneurship Center</h2>
-                    <p>wow owo w fidosjafo fjiosdajfos if you dont have any right now you can always email me but yeah if not do you have any now if not yeah we had one about traffic?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                 </div>
                 <div className="card">
                     <h2 style={{fontSize: "2.5rem"}}>By Data Systems & Technology Professionals</h2>
-                    <p>if you gys wanna come up with a new problem statement and email me you can otally do that i think that might be a smarter option as well because i think ortho k lenses are super specific so doing something broader woudl allow for more ideas for solutions</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
                 </div>
             </div>
             <img id="squiggle" src={squiggle} alt="squiggle" />
             <h1>Get to know us.</h1>
-            <p className="bodytext">this fucking TA forogt about our meeting what the fuck now you have to be nicer no this group is good they always do their work on time im gonna give them extra credit lol how much next time they try to submit something for extra credit i will lol i dont know  apoint its not a hard class you dont need the extra credit</p>
+            <p className="bodytext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             <p>
                 <Button text="Make an Account" variant="outlined" />
             </p>
