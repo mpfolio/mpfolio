62e406c722ce0472741a0e95e94f9a2a45ec5652
erika grossman <erikaagrossmann@gmail.com>
2024-10-16 20:18:01 -0400

    adding real words to launch page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <Navbar />
         <div className="section1">
             <h1 className="headline">Networking for Creatives</h1>
-            <p className="bodytext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            <p className="bodytext">My Pocketfolio is an emerging media social networking platform that provides professionals in the modeling industry with a place to showcase their portfolios, compcards, resumes, and professional experience while connecting with others.We aim to digitize and centralize the casting and networking experience in the modeling industry by providing a user-friendly discovery & networking platform. 
             </p>
             <h3>
                 <Button text="Make an Account" variant="filled" />
@@ -22,7 +22,7 @@ function App() {
             {/* TODO: the styles here are really inconsistent @sophie */}
             <h1 style={{ fontSize: "6rem" }}>Our Mission</h1>
             <h2 style={{ fontWeight: 400 }}>
-                “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.”
+            "Our mission is to save users time and money by easing the revenue making process through a user friendly professional social media platform."
             </h2>
         </div>
         <div className="section3">
@@ -30,16 +30,16 @@ function App() {
             <div className="card-container">
                 <div className="card">
                     <h2 style={{fontSize: "2.5rem"}}>Developed by models for models</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                    <p>Founder of My Pocketfolio, Nyala Tolfree is a professional model who strives to help and uplift fellow models like you.</p>
                 </div>
                 <div className="card">
                     {/* TODO: entrepreneurship is too long of a word */}
                     <h2 style={{fontSize: "2.5rem"}}>NYU Entrepreneurship Center</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                    <p>NYU’s Entrepreneurship Center backs and works with My PocketFolio. The founders dually participated in NYU’s Start-Up Bootcamp.</p>
                 </div>
                 <div className="card">
                     <h2 style={{fontSize: "2.5rem"}}>By Data Systems & Technology Professionals</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                    <p>We value your data and information, that is why My Pocketfolio is built and designed by information system specialists and technology experts.</p>
                 </div>
             </div>
             <img id="squiggle" src={squiggle} alt="squiggle" />
