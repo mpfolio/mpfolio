ac9ea59551b86d15776fb916767d0b40aaca6ab2
branmkim <bran.mkim@gmail.com>
2024-10-19 12:30:00 -0400

    home page updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7e166a505211b515"; }
+/******/ 		__webpack_require__.h = function() { return "3f1355f07382baf0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
