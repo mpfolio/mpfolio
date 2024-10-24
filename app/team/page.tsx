0feea1d292145cb1b684d789764c87ba9473bfc5
erika grossman <erikaagrossmann@gmail.com>
2024-10-24 13:49:50 -0400

    prelim changing of erika and nya about cards to center position

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -7,7 +7,7 @@ import rowan_img from "@/app/assets/team/Rowan_Headshot.jpg"
 import sophie_img from "@/app/assets/team/Sophie_Headshot.jpg"
 
 type CardProps = {
-    dir?: "left" | "right"; // position of img
+    dir?: "left" | "right" | "center"; // position of img
     name: string;
     title: string;
     img: StaticImageData;
@@ -39,22 +39,22 @@ function App() {
         <>
         <h1 className="main-title">Our Team</h1>
         <div className="card-container">
-           
+           <div className="flex-row">
                <Card 
-                dir="left"
+                dir="center"
                 name="Nyala Tolfree"
                 title="CEO"
                 img={nyala_img}
                 body="Nyala is an Undergraduate student at NYU studying Business and Technology Management. The idea of My Pocket Portfolio stemmed from her fashion model experience in New York City where she has been featured in magazines such as Harpers' Bazaar. As an Engineer and a person working in the technology industry it is glaring how archaic the arts & entertainment industry can be. Along with her team and entertainment colleagues,she has developed MPF to bring this industry into the technological era the world is in today."
             />
             <Card 
-                dir="right"
+                dir="center"
                 name="Erika Grossman"
                 title="Chief of Front End Development"
                 img={erika_img}
                 body="Erika is an Undergraduate student at NYU's Tandon School of Engineering. She is studying a major in Integrated Design and Media, and a minor in Business of Entertainment, Media, and Technology. She is experienced in graphic and front end website engineering. Specializing in front end design and development, Erika works on all of the visual aspects and visual coding segments of the website you are looking at today."
             />
-     
+     </div>
             <Card 
                 dir="left"
                 name="Rowan Tolfree"
