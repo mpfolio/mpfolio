af8b91dbf60dcd54ac1ce6b3d96178ecde78ae07
branmkim <bran.mkim@gmail.com>
2024-10-30 21:19:23 -0400

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,27 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '@/app/components/Navbar.module.css'
+import styles from '@/app/components/Footer.module.css'
 import logo from '@/app/assets/logo.svg'
 
 const Footer = () => {
     return (
         <>
-        <div>
-         
-    
+        <div className={styles.footer}>
+            <div className={styles.block}>
+                <Link href="/comingSoon">
+                    <h3>More Info</h3>
+                    <h5>About Us</h5>
+                    <h5>Contact</h5>
+                </Link>
+            </div>
+            <div className={styles.block}>
+                <Link href="/comingSoon">
+                    <h3>Help</h3>
+                    <h5>Common Questions</h5>
+                    <h5>Chat with Us</h5>
+                </Link>
+            </div>
+            <h5 className={styles.copyright}>My PocketFolio ©</h5>
         </div>
         </>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         >
             <Navbar />
             {children}
+            <Footer />
         </body>
         </html>
     );
